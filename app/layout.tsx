ce76df20c25cd9437d26b1c8a47f9a37661038de
Andrew Lobban <andrewglobban@gmail.com>
2024-12-07 23:31:13 -0600

    Override default font-family

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 import './globals.css';
 import NavBar from '@/components/NavBar';
-import { orbitron } from './fonts';
+import { exo2, orbitron } from './fonts';
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,7 +10,7 @@ interface LayoutProps {
 
 export default function RootLayout({ children }: LayoutProps) {
   return (
-    <html lang="en" className={orbitron.variable}>
+    <html lang="en" className={`${exo2.variable} ${orbitron.variable}`}>
       <body className="bg-orange-50 flex flex-col min-h-screen px-4 py-2">
         <header>
           <NavBar />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       fontFamily: {
         orbitron: ['var(--font-orbitron)', 'sans-serif'],
+        sans: ['var(--font-exo_2)', 'sans-serif'],
       },
     },
   },
