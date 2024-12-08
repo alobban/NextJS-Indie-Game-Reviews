b21a0610bfd881cc7580f857e7e8dbd5cbdef943
Andrew Lobban <andrewglobban@gmail.com>
2024-12-07 23:20:16 -0600

    Add Custom Tailwind Font

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -6,9 +6,5 @@ export interface HeadingProps {
 }
 
 export default function Heading({ children }: HeadingProps) {
-  return (
-    <h1 className={`font-bold pb-3 text-2xl ${orbitron.className}`}>
-      {children}
-    </h1>
-  );
+  return <h1 className="font-bold font-orbitron pb-3 text-2xl">{children}</h1>;
 }
