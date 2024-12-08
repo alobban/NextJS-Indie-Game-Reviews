640f4742b0282ed8d4b8e2dc41621e8e94481ac7
Andrew Lobban <andrewglobban@gmail.com>
2024-12-07 21:32:08 -0600

    Configure & Import Tailwind Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./app/**/*.{jsx,tsx}', './components/**/*.{jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
