edbac1ec11a695393816f2617e1cb2cf08091f7a
Andrew Lobban <andrewglobban@gmail.com>
2024-12-07 22:19:11 -0600

    Add static content

--- app/reviews/hollow-knight/page.tsx
+++ app/reviews/hollow-knight/page.tsx
@@ -4,6 +4,13 @@ export default function HollowKnightPage() {
   return (
     <>
       <Heading>Hollow Knight</Heading>
+      <img
+        src="/images/hollow-knight.jpg"
+        alt=""
+        width={640}
+        height={360}
+        className="rounded mb-2"
+      />
       <p>This will be the review for Hollow Knight.</p>
     </>
   );
--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -4,6 +4,13 @@ export default function StardewValleyPage() {
   return (
     <>
       <Heading>Stardew Valley</Heading>
+      <img
+        src="/images/stardew-valley.jpg"
+        alt=""
+        width={640}
+        height={360}
+        className="rounded mb-2"
+      />
       <p>This will be the review for Stardew Valley.</p>
     </>
   );
