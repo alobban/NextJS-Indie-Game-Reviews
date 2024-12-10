4ed678a9c29d29ba61e9821999b8d4a8b01c706d
Andrew Lobban <andrewglobban@gmail.com>
2024-12-09 20:40:36 -0600

    Accessiblity for Screen Readers

--- components/PaginationBar.tsx
+++ components/PaginationBar.tsx
@@ -6,6 +6,7 @@ export default function PaginationBar({ href, page, pageCount }) {
     <div className="flex gap-2 pb-3">
       <PaginationLink href={`${href}?page=${page - 1}`} enabled={page > 1}>
         <ChevronLeftIcon className="h-5 w-5" />
+        <span className="sr-only">Previous Page</span>
       </PaginationLink>
       <span>
         Page {page} of {pageCount}
@@ -15,6 +16,7 @@ export default function PaginationBar({ href, page, pageCount }) {
         enabled={page < pageCount}
       >
         <ChevronRightIcon className="h-5 w-5" />
+        <span className="sr-only">Next Page</span>
       </PaginationLink>
     </div>
   );
